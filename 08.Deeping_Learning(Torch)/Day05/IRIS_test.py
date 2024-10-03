629179294d6d3e562e23325dd05b54af18854adb
Un2duck <dkfl2003@naver.com>
2024-10-03 22:40:58 +0900

    2024.10.03-3

---
+++ 08.Deeping_Learning(Torch)/Day05/IRIS_test.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append(r'C:\Users\KDP-50\OneDrive\바탕 화면\Python06\08.Deeping_Learning(Torch)\DL_Class')
+import DL_iris
+
+# ---------------------------------------------------------------------
+# 모델 정보 확인
+# ---------------------------------------------------------------------
+
+
+# Reg_model = DL.irisRegModel()
+# print(Reg_model)
+# DL.summary(Reg_model, input_size=(10,3))
+
+# BCF_model = DL.irisBCFModel()
+# print(BCF_model)
+# DL.summary(BCF_model, input_size=(50,4))
+
+# MCF_model = DL.irisMCFModel()
+# print(MCF_model)
+# summary(MCF_model, input_size=(20,4))
+
+# ---------------------------------------------------------------------
